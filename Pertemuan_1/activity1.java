845edcc02158198bf9a93d661929a9406e942521
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-10-15 11:59:59 +0700

    Add activity1 class with name, npm, and address variables

---
+++ Pertemuan_1/activity1.java
@@ -0,0 +1,20 @@
+package Pertemuan_1;
+
+public class activity1 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String nama;
+        int npm;
+        String alamat;
+
+        // Inisialisasi variabel
+        nama = "Muhamad Ibnu Khaidar Hafiz";
+        npm = 50421867;
+        alamat = "Jl. Kecapi 1, No 52";
+
+        // print variabel
+        System.out.println("Nama saya adalah " + nama);
+        System.out.println("NPM saya adalah " + npm);
+        System.out.println("Alamat saya adalah " + alamat);
+    }
+}
