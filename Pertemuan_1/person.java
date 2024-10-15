757633506026f65aa12a098e43056b676129abb6
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-10-15 11:54:13 +0700

    Add person class with name, age, and job variables

---
+++ Pertemuan_1/person.java
@@ -0,0 +1,19 @@
+package Pertemuan_1;
+
+public class person {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String name;
+        int age;
+        String job;
+
+        // Inisialisasi variabel
+        name = "Muhamad Ibnu Khaidar Hafiz";
+        age = 22;
+        job = "Mahasiswa";
+
+        System.out.println("Nama saya adalah " + name);
+        System.out.println("Saya berumur " + age + " tahun");
+        System.out.println("Saya adalah seorang " + job);
+    }
+}
