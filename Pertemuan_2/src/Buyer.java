bde7a632968f991bc04ce1e80bba1f583343eb6c
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-10-22 08:52:06 +0700

    created buyer class

---
+++ Pertemuan_2/src/Buyer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ibnuk - Muhamad Ibnu Khaidar Hafiz - 50421867 - 4IA15
+ */
+public class Buyer extends User{
+    public void login() {
+        System.out.println("Buyer Berhasil Login");
+    }
+    
+    public void logout() {
+        System.out.println("Buyer Berhasil Logout");
+    }
+}
