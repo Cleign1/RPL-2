9f9b5dd1cf7a08f6ea8eb90ccd67fa300fb01fce
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-10-22 08:51:52 +0700

    created user class

---
+++ Pertemuan_2/src/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ibnuk - Muhamad Ibnu Khaidar Hafiz - 50421867 - 4IA15
+ */
+public class User {
+    public void login() {
+        System.err.println("Selamat Anda Berhasil Login");
+    }
+    
+    public void logout() {
+        System.err.println("Anda Berhasil Melakukan Logout");
+    }
+}
