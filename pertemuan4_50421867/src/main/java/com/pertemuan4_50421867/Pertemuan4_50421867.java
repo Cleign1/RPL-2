c541c1d0060f8f88f4f138e1b2acf8b4cf950768
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-11-05 09:33:06 +0700

    Add Pertemuan4_50421867.java with a basic Hello World implementation

---
+++ pertemuan4_50421867/src/main/java/com/pertemuan4_50421867/Pertemuan4_50421867.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pertemuan4_50421867;
+
+/**
+ *
+ * @author ibnuk
+ */
+public class Pertemuan4_50421867 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
