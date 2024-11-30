66026d8b6715496095281055ae5b275c3af14cd4
Cleign1 <ibnukhaidar@gmail.com>
2024-12-01 01:23:12 +0700

    added Jframe but not done

---
+++ kasir/src/main/java/com/ujianrpl2/kasir/view/MainView_Jframe.java
@@ -0,0 +1,9 @@
+package com.ujianrpl2.kasir.view;
+
+import javax.swing.*;
+
+public class MainView_Jframe {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTable table1;
+}
