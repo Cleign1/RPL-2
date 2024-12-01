8316906bfe107aa75275e515373f877bcb78f3d8
Cleign1 <ibnukhaidar@gmail.com>
2024-12-01 13:14:07 +0700

    Delete MainView_Jframe.java

--- kasir/src/main/java/com/ujianrpl2/kasir/view/MainView_Jframe.java
+++
@@ -1,9 +0,0 @@
-package com.ujianrpl2.kasir.view;
-
-import javax.swing.*;
-
-public class MainView_Jframe {
-    private JTabbedPane tabbedPane1;
-    private JPanel panel1;
-    private JTable table1;
-}
